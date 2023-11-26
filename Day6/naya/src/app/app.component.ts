ba50c53d6e843c9b8091aa3b5b7178dcbeaafa7b
samratcodes <samratcodess@gmail.com>
2023-11-26 21:58:44 +0545

    day 7

---
+++ Day6/naya/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  items= [
+    new WishItem('Go to the store'),
+    new WishItem('Get eggs', false),
+    new WishItem('Get milk', false),
+  ];
+  title = 'naya';
+}
